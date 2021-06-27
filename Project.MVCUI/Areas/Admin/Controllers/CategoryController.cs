e696290d4c228e09c8a932197a359bf9bf13dc42
etkayildiz <82145834+etkayildiz@users.noreply.github.com>
2021-06-27 18:40:13 +0300

    Admin Area CategoryController - View eklendi

---
+++ Project.MVCUI/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,63 @@
+﻿using Project.BLL.DesignPatterns.GenericRepository.ConcRep;
+using Project.ENTITIES.Models;
+using Project.MVCUI.VMClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Project.MVCUI.Areas.Admin.Controllers
+{
+    public class CategoryController : Controller
+    {
+        CategoryRep _cRep;
+        public CategoryController()
+        {
+            _cRep = new CategoryRep();
+        }
+        // GET: Admin/Category
+        public ActionResult CategoryList(int? id)
+        {
+            CategoryVM cvm = id == null ? new CategoryVM
+            {
+                Categories = _cRep.GetActives()
+            } : new CategoryVM { Categories = _cRep.Where(x => x.ID == id) };
+            return View(cvm);
+        }
+
+        public ActionResult AddCategory()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddCategory(Category category)
+        {
+            _cRep.Add(category);
+            return RedirectToAction("CategoryList");
+        }
+
+        public ActionResult UpdateCategory(int id)
+        {
+            CategoryVM cvm = new CategoryVM
+            {
+                Category = _cRep.Find(id)
+            };
+            return View(cvm);
+        }
+
+        [HttpPost]
+        public ActionResult UpdateCategory(Category category)
+        {
+            _cRep.Update(category);
+            return RedirectToAction("CategoryList");
+        }
+
+        public ActionResult DeleteCategory(int id)
+        {
+            _cRep.Delete(_cRep.Find(id));
+            return RedirectToAction("CategoryList");
+        }
+    }
+}
