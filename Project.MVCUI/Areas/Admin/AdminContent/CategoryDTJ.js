c265861198d0d3fc6df52127a776b5b209d3e16e
Tuğberk <84016118+TugberkMehdioglu@users.noreply.github.com>
2021-07-21 10:25:17 +0300

    CategoryDTJ düzenlendi

--- Project.MVCUI/Areas/Admin/AdminContent/CategoryDTJ.js
+++ Project.MVCUI/Areas/Admin/AdminContent/CategoryDTJ.js
@@ -3,7 +3,6 @@
     var sOut = '<table cellpadding="5" cellspacing="0" border="0" style="padding-left:50px;">';
     sOut += '<tr><td>Ürünleri:</td><td> <a title="İçerik" href="/Admin/Product/ProductList/' + aData[1] + '">' + aData[2] + ' </a>  </td></tr>';
     sOut += '<tr><td>Detay Linki:</td><td><a title="Detay" href="/Admin/Category/CategoryList/' + aData[1] + '">' + aData[2] + ' detay </a></td></tr>';
-    sOut += '<tr><td>Kategori Ekleme:</td><td> <a href="/Admin/Category/AddCategory" class="btn btn-primary"> Kategori Ekle </a>  </td></tr>';
     sOut += '</table>';
 
     return sOut;
